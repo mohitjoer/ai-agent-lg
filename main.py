2627adf265379b8735b52b0bf74dbef6a701cc5c
mohitjoer <mohitjoe.r@gmail.com>
2025-11-19 23:34:47 +0530

    commit	new file:   .gitignore commit	new file:   .python-version commit	new file:   README.md commit	new file:   main.py commit	new file:   pyproject.toml commit	new file:   uv.lock

---
+++ main.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from typing import Annotated , Literal
+from langgraph.graph import StateGraph , START , END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel , Field
+from typing_extensions import TypedDict
+
+load_dotenv()
+
+llm =  init_chat_model("gpt-4")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messsages":[llm.invoke(state["messages"])]}
+
+
+
+graph_builder.add_node("chatbot",chatbot)
+graph_builder.add_edge( start_key=START , end_key="chatbot" )
+graph_builder.add_edge( start_key="chatbot", end_key=END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter A Message:")
+state = graph.invoke({"messages":[{"role":"user","content":user_input}]})
+
+print(state["messages"][-1].content)
